7554d36c83f4fe4ab4104f43164b055e027a16c9
Roo <147157842+RooDunnill@users.noreply.github.com>
2025-04-17 23:30:58 +0100

    Create ssh_log.sh

---
+++ ssh_log.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+if [ -n "$SSH_CONNECTION" ]; then
+  HOSTNAME=$(hostname)
+  CLIENT_IP=$(echo $SSH_CONNECTION | awk '{print $1}')
+  SERVER_IP=$(hostname -I | awk '{print $1}')
+  USERNAME=$(whoami)
+  TIME=$(date '+%Y-%m-%d %H:%M:%S')
+  WEBHOOK_FILE="/home/$USERNAME/config/discord_webhooks/$HOSTNAME"
+  
+  [ -f "$WEBHOOK_FILE" ] || exit 0
+  WEBHOOK_URL=$(cat "$WEBHOOK_FILE")
+
+  MESSAGE="[$HOSTNAME SSH ALERT]
+
+User: $USERNAME
+From: $CLIENT_IP
+To: $SERVER_IP
+Time: $TIME"
+
+  curl -s -H "Content-Type: application/json" \
+       -X POST \
+       -d "{\"username\": \"$HOSTNAME\", \"content\": \"$MESSAGE\"}" \
+       "$WEBHOOK_URL" > /dev/null
+fi
